7ea1768ac7851af69ec992eb1c120c817a8ba08d
heyyee <xy004@umd.edu>
2023-03-17 13:04:35 -0400

    Lab 6: Broken Load button

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,9 +1,4 @@
-/* eslint-disable max-len */
-
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
+/* eslint-disable max-len *
 
 /*
   ## Utility Functions
@@ -11,8 +6,21 @@
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 function injectHTML(list) {
   console.log('fired injectHTML');
+  const target = document.querySelector('#restaurant_list');
+  target.innerHTML = '';
+
+  list.forEach((item) => {
+    const str = `<li>${item.name}</li>`; /* `` bring variable in and render as str*/
+    target.innerHTML += str;
+  })
   /*
   ## JS and HTML Injection
     There are a bunch of methods to inject text or HTML into a document using JS
@@ -28,6 +36,60 @@ function injectHTML(list) {
     - Display the name of that restaurant and what category of food it is
 */
 }
+
+function filterList(list, query){ //query is a value user input
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery); //includes evaluates == 
+  }) 
+}
+
+
+async function mainEvent() { // the async keyword means we can make API requests
+  const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button');
+  
+  let currentList = [];
+  
+  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+    submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
+    console.log('form submission'); // this is substituting for a "breakpoint"
+
+    // this is the preferred way to handle form data in JS in 2022
+    const formData = new FormData(submitEvent.target); // get the data from the listener target
+    const formProps = Object.fromEntries(formData); // Turn it into an object
+
+    //get request to control results
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+
+    // This changes the response from the GET into data we can use - an "object"
+    currentList = await results.json();
+    console.table(currentList); // this is called "dot notation"
+
+  });
+
+  filterButton.addEventListener('click', (event) => {
+    console.log('clicked FilterButton');
+
+    const formData = new FormData(form);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+  })
+
+}
+//add event listener
+
+/*
+  This adds an event listener that fires our main event only once our page elements have loaded
+  The use of the async keyword means we can "await" events before continuing in our scripts
+  In this case, we load some data when the form has submitted
+*/
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
 
 function processRestaurants(list) {
   console.log('fired restaurants list');

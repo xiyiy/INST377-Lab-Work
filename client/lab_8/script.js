ee00b5ea3b6835877e33f5f190a89f08ffc5bd05
heyyee <xy004@umd.edu>
2023-04-15 17:58:43 -0400

    Broken Clear Button Lab 8

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -39,22 +39,50 @@ function cutRestaurantList(list) {
       return list[index];
     }));
 }
-  
+
+function initMap(){
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+    return carto;
+}
+
+function markerPlace(array, map) {
+    console.log('array for markers', array);
+
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+    });
+
+    array.forEach((item) => {
+        console.log('markerPlace', item);
+        const {coordinates} = item.geocoded_column_1;
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    });
+}
+
 async function mainEvent() {
     // the async keyword means we can make API requests
     const form = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
     const loadDataButton = document.querySelector("#data_load");
+    const clearDataButton = document.querySelector("#data_clear");
     const generateListButton = document.querySelector("#generate");
     const textField = document.querySelector("#resto");
   
     const loadAnimation = document.querySelector("#data_load_animation");
     loadAnimation.style.display = "none";
     generateListButton.classList.add("hidden")
+
+    const carto = initMap();
   
-    const storedData = localStorage.getItem('storedData')
-    let parsedData = JSON.parse(storedData)
-    if (parsedData.length > 0) {
-      generateListButton.classList.remove('hidden')
+    const storedData = localStorage.getItem("storedData");
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0) {
+      generateListButton.classList.remove("hidden")
     }
     
     let currentList = [];
@@ -72,12 +100,12 @@ async function mainEvent() {
       // This changes the response from the GET into data we can use - an "object"
       const storedList = await results.json();
       localStorage.setItem("storedData", JSON.stringify(storedList));
-     // parsedData = storedList;
+      parsedData = storedList;
 
-      /*if (parsedData.length > 0) {
-        generateListButton.classList.remove('hidden')
-      }*/
-
+      if (parsedData?.length > 0) {
+        generateListButton.classList.remove("hidden")
+      }
+      
       loadAnimation.style.display = "none";
       //console.table(storedList); // this is called "dot notation"
     });
@@ -87,6 +115,7 @@ async function mainEvent() {
       currentList = cutRestaurantList(parsedData);
       console.log(currentList);
       injectHTML(currentList);
+      markerPlace(currentList, carto);
     });
   
     textField.addEventListener("input", (event) => {
@@ -94,6 +123,13 @@ async function mainEvent() {
       const newList = filterList(currentList, event.target.value);
       console.log(newList);
       injectHTML(newList);
+      markerPlace(newList, carto);
+    });
+
+    clearDataButton.addEventListener('click', (event) => {
+        console.log('clear browser data');
+        localStorage.clear();
+        console.log('localStorage Check', localStorage.getItem("storedData"));
     });
 }
 //add event listener
